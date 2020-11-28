d51c70f0ce74fe4e7317ffb911115a702a119e2a
nmu <nmu2312@gmail.com>
2020-11-28 17:13:40 +0900

    Generate HTML through Pug

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,21 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.pug/,
+                use: [
+                    {
+                        loader: 'html-loader',
+
+                    },
+                    {
+                        loader: 'pug-html-loader',
+                        options: {
+                            pretty: true
+                        }
+                    },
+                ],
+            },
         ],
     },
     plugins: [
@@ -42,7 +57,12 @@ module.exports = {
             filename: './stylesheets/main.css',
         }),
         new HtmlWebpackPlugin({
-            template: './src/templates/index.html',
+            template: './src/templates/index.pug',
+            filename: 'index.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/templates/access.pug',
+            filename: 'access.html',
         }),
         new CleanWebpackPlugin(),
     ]
